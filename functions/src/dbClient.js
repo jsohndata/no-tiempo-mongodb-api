6100d327c1eceabfcc56883edb8b7ea780b60039
jSohnData <jihos@me.com>
2023-05-06 13:11:29 -0400

    ADDED comment for students

--- functions/src/dbClient.js
+++ functions/src/dbClient.js
@@ -7,8 +7,15 @@ const { DB_NAME } = process.env;
 const { COLLECTION_NAME } = process.env;
 
 export async function getDbClient() {
+  // Create new MongoClient instance
   const mongo = new MongoClient(MONGO_URI);
+  
+  // Connect to the MongoDB cluster
+  /* await: connect() returns a promise, which resolves when the connection is established. */
   await mongo.connect();
+
+  // Get the database instance
+  /* Selects the database with db method from MongoClient */
   const dbClient = mongo.db(DB_NAME);
   
   return ({ 
