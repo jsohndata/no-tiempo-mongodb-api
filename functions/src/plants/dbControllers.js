e593d81a9153b160ab1df773e5363ce4c3a0b548
jSohnData <jihos@me.com>
2023-05-09 16:37:02 -0400

    UPDATED sort, filter and code clean up

--- functions/src/plants/dbControllers.js
+++ functions/src/plants/dbControllers.js
@@ -4,21 +4,27 @@ import { ObjectId } from "mongodb";
 const { dbClient, collectionName } = await getDbClient();
 const errMessage = { message: "💩oopsy! An error occurred." };
 
+// Get the collection
+const collectionClient = dbClient.collection(collectionName);
+
+
+const getSortBy = (sort) => {
+  // Check if the sort order is asc or desc
+  const sortOrder = sort.startsWith("-") ? -1 : 1; 
+  
+  // Remove the "-" sign from the sort param to get the field to sort by
+  const sortBy = sort.slice(1);
+
+  return {sortBy,sortOrder};
+}
+
 // Get: All Docs
 /* *********************** */
 export async function getAllDocs(req, res) {
   const sort = req.query.sort || "_id";
-
-  // Check if the sort order is asc or desc
-  const sortOrder = sort.startsWith("-") 
-    ? -1 
-    : 1; 
-  
-  // Remove the "-" sign from the sort param to get the field to sort by
-  const sortBy = sort.slice(1);     
+  const {sortBy,sortOrder} = getSortBy(sort);
      
-  const collection = await dbClient
-    .collection(collectionName)
+  const collection = await collectionClient
     .find({})
     .sort( {[sortBy]: sortOrder} )
     .limit(10)
@@ -35,34 +41,35 @@ export async function getAllDocs(req, res) {
 export async function getDocById(req, res) {
   const docId = new ObjectId(req.params.id);
 
-  const collection = await dbClient
-    .collection(collectionName)
+  const collection = await collectionClient
     .findOne( {_id: docId} )
     .catch(err => res.status(500).json(errMessage));
   
   console.table(collection);
-  res.status(200).json( {data: collection} );
+  res.status(200).json(collection);
 }
 
 
 // Get: Doc by Filter
 /* *********************** */
 export async function getDocByFilter(req, res) {
+  const sort = req.query.sort || "_id";
+  const {sortBy,sortOrder} = getSortBy(sort);
+
   const {filterType} = req.params;
   const {filterValue} = req.params;
 
   console.log("=======>", filterType);
   console.log("=======>", filterValue);
 
-  const collection = await dbClient
-    .collection(collectionName)
-    .find( {[filterType] : filterValue} )
+  const collection = await collectionClient
+    .find( {[filterType]: filterValue} )
+    .sort( {[sortBy]: sortOrder} )
     .toArray()
     .catch(err => res.status(500).json(errMessage));
 
-  
-  console.table({collection});
-  res.status(200).json({ data: collection });
+  console.table( {collection} );
+  res.status(200).json(collection);
 }
 
 
@@ -71,14 +78,13 @@ export async function getDocByFilter(req, res) {
 export async function createDoc(req, res) {
   const docContent = req.body;
 
-  const collection = await dbClient
-    .collection(collectionName)
+  const collection = await collectionClient
     .insertOne(docContent)
     .catch(err => res.status(500).json(errMessage));
   
-    console.table({collection});
-    res.status(200).json({ data: collection });
-}
+    console.table( {collection} );
+    res.status(200).json(collection);
+  }
 
 
 // Delete: Doc by Id
@@ -86,15 +92,13 @@ export async function createDoc(req, res) {
 export async function deleteDoc(req, res) {
   const docId = new ObjectId(req.params.id);
 
-  const collection = await dbClient
-    .collection(collectionName)
-    .deleteOne(
-      { _id: docId })
+  const collection = await collectionClient
+    .deleteOne( {_id: docId} )
     .catch(err => res.status(500).json(errMessage));
     
     console.table({collection});
-    res.status(200).json({ data: collection });
-}
+    res.status(200).json(collection);
+  }
 
 
 // Put: Update Doc by Id
@@ -103,13 +107,12 @@ export async function updateDoc(req, res) {
   const docId = new ObjectId(req.params.id);
   const docContent = req.body;
 
-  const collection = await dbClient
-    .collection(collectionName)
+  const collection = await collectionClient
     .updateOne(
-      { _id: docId }, 
-      { $set: docContent })
+      {_id: docId}, 
+      {$set: docContent} )
     .catch(err => res.status(500).json(errMessage));
   
-    console.table({collection});
-    res.status(200).json({ data: collection });
-}
+    console.table( {collection} );
+    res.status(200).json(collection);
+  }
